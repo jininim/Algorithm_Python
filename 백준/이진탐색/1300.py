bcce113b428284fb3ebbe5fe6e9cf1bc1b9d67d3
developer Jin <91578450+jininim@users.noreply.github.com>
2023-01-06 22:14:40 +0900

    이진탐색 백준 1300번

--- 백준/이진탐색/1300.py
+++ 백준/이진탐색/1300.py
@@ -1,10 +1,22 @@
 n = int(input())  # 배열의 크기
 k = int(input())
 
-start = 1
-end = k
 
-while start<= end:
-    mid = (start +end )// 2
+def bianry_search(target, start, end):
+    while start <= end:
+        # 중간값
+        mid = (start + end) // 2
+
+        # 내가 원하는 인덱스의 값
+        cnt = 0
+        for i in range(1, n + 1):
+            cnt += min(mid // i, n)
+
+        if cnt >= target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return start
 
 
+print(bianry_search(k, 1, n * n))
